c842a47dff73a90795dee0462178860fc522ff6c
NikBabukhin <6opsh50@gmail.com>
2022-02-17 16:17:53 +0300

    add task Hello World

---
+++ Tasks/HelloWorld/index.js
@@ -0,0 +1,5 @@
+const helloWorld = () => {
+    return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33);
+    // feel free to change this into a function :)
+};
+console.log(helloWorld());
