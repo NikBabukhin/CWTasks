05adba98b39581fad6939e0797235f823c88e4a3
NikBabukhin <6opsh50@gmail.com>
2022-02-24 13:11:03 +0300

    fix task Split string

--- Tasks/SplitString/index.js
+++ Tasks/SplitString/index.js
@@ -1,4 +1,14 @@
 function solution(str){
  let bestArr = str.split('');
- bestArr;
-}
+ let finalArr = [];
+ for (i=0;i<bestArr.length;i=i+2) {
+    if (bestArr[i+1] === undefined) {
+        finalArr.push(bestArr[i] + '_')
+    } else {
+        finalArr.push(bestArr[i] + bestArr[i+1]);
+    };
+ };
+ return finalArr;
+};
+
+console.log(solution('abcdi'));
